42c4a36575d94519ffe03580ecfb94d47f1b313d
Jason-Brk <jason.braeker@bluewin.ch>
2020-12-08 10:04:04 +0100

    upload

---
+++ Programme/Jason Bräker/Tetris/TBlock.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TBlock here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TBlock extends Blocks
+{
+    /**
+     * Act - do whatever the TBlock wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
