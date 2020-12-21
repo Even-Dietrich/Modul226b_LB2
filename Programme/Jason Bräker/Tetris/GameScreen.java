039b556e1c7981d45ac1cd6a3a4a8561060853e9
Jason-Brk <jason.braeker@bluewin.ch>
2020-12-21 10:16:44 +0100

    upload

--- Programme/Jason Bräker/Tetris/GameScreen.java
+++ Programme/Jason Bräker/Tetris/GameScreen.java
@@ -15,8 +15,9 @@ public class GameScreen extends World
      */
     public GameScreen()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(500, 600, 1);
+        getBackground().setColor(Color.GREY);
+        getBackground().fill();
         prepare();
     }
     
